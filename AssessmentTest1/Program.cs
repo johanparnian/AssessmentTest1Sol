eddd4401121b6b3535cda110ae9cea2b7e01fed5
Johan P <Johan@DESKTOP-M4MSCGK>
2022-07-11 08:44:05 +0200

    Add project files.

---
+++ AssessmentTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssessmentTest1 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
